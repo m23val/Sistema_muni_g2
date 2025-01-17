bd8526867eb6021cf0e248c4edc836144c06c801
2Mari47 <123851719+2Mari47@users.noreply.github.com>
2025-01-17 08:31:21 -0500

    Add files via upload

--- ui/main_window.py
+++ ui/main_window.py
@@ -4,6 +4,7 @@ from db.connection import get_connection
 from datetime import datetime
 from PIL import Image, ImageTk
 
+
 class MainWindow:
     def __init__(self, root):
         self.root = root
@@ -12,7 +13,7 @@ class MainWindow:
         self.root.configure(bg="#FFFFFF")
 
         # Cambiar el ícono de la ventana
-        icon_path = r"C:\Users\Max\Desktop\SISTEMA MUNI\assets\logo.ico"  # Ruta del archivo .ico
+        icon_path = r"C:\Users\marie\OneDrive\Escritorio\SISTEMA\Sistema_muni_g2\SISTEMA MUNI\assets\logo.ico"
         try:
             self.root.iconbitmap(icon_path)
         except Exception as e:
@@ -21,7 +22,8 @@ class MainWindow:
 
         # Cargar y mostrar la imagen
         try:
-            image_path = r"C:\Users\Max\Desktop\SISTEMA MUNI\assets\logo.png"  # Asegúrate de usar la extensión correcta
+            #image_path = r"C:\Users\Max\Desktop\SISTEMA MUNI\assets\logo.png"  # Asegúrate de usar la extensión correcta
+            image_path = r"C:\Users\marie\OneDrive\Escritorio\SISTEMA\Sistema_muni_g2\SISTEMA MUNI\assets\logo.png"  # Asegúrate de usar la extensión correcta
             image = Image.open(image_path)
             image = image.resize((150, 150), Image.Resampling.LANCZOS)  # Corregido LANCZOS
             self.logo = ImageTk.PhotoImage(image)
@@ -33,44 +35,122 @@ class MainWindow:
         # Etiqueta de bienvenida
         tk.Label(root, text="Bienvenido", font=("Arial", 20), bg="#FFD700").pack(pady=10)
 
-        # Campo para ingresar DNI
+        #Opciones de área
+        self.motivo_var=tk.StringVar()
+        self.motivo_var.set("Seleccionar")
+        motivo_options = ["Consultas", "Deudas", "Documentos", "Multas", "Pagos"]
+
+        motivo_label = tk.Label(root, text="Seleccionar Motivo", font=("Arial",14), bg="#FFFFFF")
+        motivo_label.pack(pady=5)
+
+        motivo_menu = tk.OptionMenu(root, self.motivo_var, *motivo_options)
+        motivo_menu.config(font=("Arial",14))
+        motivo_menu.pack(pady=10)
+
+        #Campo para ingresar DNI (después de escoger el área)
+        self.dni_label = tk.Label(root, text = "Ingrese su DNI", font=("Arial", 14), bg="#FFFFFF")
         self.dni_entry = tk.Entry(root, font=("Arial", 14))
-        self.dni_entry.pack(pady=20)
+        self.dni_button = tk.Button(root, text="Generar Turno", bg="#008000", fg="white", font=("Arial", 14), command=self.generar_turno)
+        self.dni_button.pack_forget()
+
+        #Iniciar el botón y DNI pero deshabilitados
+        self.dni_label.pack_forget()
+        self.dni_entry.pack_forget()
+        self.dni_button.pack_forget()
+
+        #Solo cuando se seleccione el área, se muestra el campo DNI
+        self.motivo_var.trace_add("write", self.mostrar_dni)
+         # Establecer texto por defecto en el campo de DNI
         self.dni_entry.insert(0, "Ingrese su DNI")
+        self.dni_entry.bind("<FocusIn>", self.borrar_texto_dni)
+        self.dni_entry.bind("<FocusOut>", self.reemplazar_texto_dni)
+#############################################################################################
+    def borrar_texto_dni(self, event):
+        """Borra el texto por defecto cuando el campo tiene foco."""
+        if self.dni_entry.get() == "Ingrese su DNI":
+            self.dni_entry.delete(0, tk.END)
+#########################################################################################
+    def reemplazar_texto_dni(self, event):
+        """Vuelve a poner el texto por defecto si el campo está vacío."""
+        if self.dni_entry.get() == "":
+            self.dni_entry.insert(0, "Ingrese su DNI")
+        
+###############################################################################################
+    def obtener_siguiente_numero(self, cursor, prefijo):
+        prefijo_busqueda = f"{prefijo}%"
+        cursor.execute("SELECT numero_turno FROM turnos WHERE numero_turno LIKE %s ORDER BY id DESC LIMIT 1", (prefijo_busqueda,))       
 
-        # Botón para generar turno
-        tk.Button(root, text="Generar Turno", bg="#008000", fg="white", font=("Arial", 14),
-                  command=self.generar_turno).pack(pady=10)
-
-    def obtener_siguiente_numero(self, cursor):
-        cursor.execute("SELECT numero_turno FROM turnos ORDER BY id DESC LIMIT 1")
         ultimo_turno = cursor.fetchone()
         if ultimo_turno:
             ultimo_numero = int(ultimo_turno[0].split('-')[1])
             siguiente_numero = ultimo_numero + 1
         else:
             siguiente_numero = 1
-        return f"T-{siguiente_numero:04d}"
+        return f"{prefijo}-{siguiente_numero:04d}"
+    
+###############################################################################################
+    def mostrar_dni(self, *args):
+        motivo = self.motivo_var.get()
+        if motivo != "Seleccionar":
+            self.dni_label.pack(pady=10)
+            self.dni_entry.pack(pady=20)
+            self.dni_button.pack(pady=10)
 
+            # Limpia el campo de DNI y muestra el texto por defecto
+            self.dni_entry.delete(0, tk.END)
+            self.dni_entry.insert(0, "Ingrese su DNI")  # Siempre restablece el texto por defecto
+        else:
+            # Se oculta el campo de DNI si no se selecciona área
+            self.dni_label.pack_forget()
+            self.dni_entry.pack_forget()
+            self.dni_button.pack_forget()
+
+
+
+###############################################################################################
     def generar_turno(self):
         dni = self.dni_entry.get()
+        motivo = self.motivo_var.get()
+
         if len(dni) != 8 or not dni.isdigit():
             messagebox.showerror("Error", "DNI inválido")
             return
-
-        area = "Trámites Generales"  # Esto podría ser dinámico según el sistema
+        # Determinar el prefijo según el área seleccionada
+        if motivo == "Consultas":
+            prioridad = "Muy baja"
+            prefijo = "C"
+        elif motivo == "Deudas":
+            prioridad = "Media"
+            prefijo = "S"
+        elif motivo == "Documentos":
+            prioridad = "Baja"
+            prefijo = "D"
+        elif motivo == "Multas":
+            prioridad = "Muy Alta"
+            prefijo = "M"
+        elif motivo == "Pagos":
+            prioridad = "Alta"
+            prefijo = "P"
+        else:
+            messagebox.showerror("Error", "Motivo no válida")
+            return
+        
+       
+        #area = "Trámites Generales"  # Esto podría ser dinámico según el sistema
         hora_actual = datetime.now().strftime("%H:%M:%S")  # Hora en formato HH:MM:SS
 
+###############################################################################################
         connection = get_connection()
         if connection:
             try:
                 cursor = connection.cursor()
-                numero_turno = self.obtener_siguiente_numero(cursor)
+                numero_turno = self.obtener_siguiente_numero(cursor, prefijo)
                 cursor.execute("""
-                    INSERT INTO turnos (dni, numero_turno, estado, fecha_hora) 
-                    VALUES (%s, %s, 'espera', CURRENT_TIMESTAMP) 
+                    INSERT INTO turnos (dni, numero_turno, estado, fecha_hora, prioridad) 
+                    VALUES (%s, %s, 'espera', CURRENT_TIMESTAMP, %s) 
                     RETURNING numero_turno
-                """, (dni, numero_turno))
+                """, (dni, numero_turno, prioridad))
+
                 turno = cursor.fetchone()[0]
                 connection.commit()
 
@@ -80,14 +160,15 @@ class MainWindow:
                     f"Municipalidad de Nuevo Chimbote\n"
                     f"----------------------------------------------\n"
                     f"Turno: {turno}\n"
+                    f"Motivo: {motivo}\n"
                     f"DNI: {dni}\n"
-                    f"Área: {area}\n"
                     f"Hora: {hora_actual}\n"
                     f"----------------------------------------------\n"
                     f"Por favor espere su turno.\n"
                     f"Gracias por su paciencia.\n"
                 )
                 messagebox.showinfo("Turno Generado", mensaje)
+
 
                 self.dni_entry.delete(0, tk.END)
                 self.dni_entry.insert(0, "Ingrese su DNI")
@@ -99,6 +180,9 @@ class MainWindow:
                 connection.close()
         else:
             messagebox.showerror("Error", "No se pudo conectar a la base de datos")
+        
+    
+
 
 # Inicia la aplicación
 if __name__ == "__main__":
