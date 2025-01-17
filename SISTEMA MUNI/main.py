7fd44ba3e54d682858592e4daa4bc59bef73395b
2Mari47 <123851719+2Mari47@users.noreply.github.com>
2025-01-17 08:35:00 -0500

    Add files via upload

--- SISTEMA MUNI/main.py
+++ SISTEMA MUNI/main.py
@@ -1,7 +1,8 @@
 from tkinter import Tk
-from ui.main_window import MainWindow
+from ui.menu_general import MenuGeneral
 
 if __name__ == "__main__":
     root = Tk()
-    app = MainWindow(root)
+    #app = MainWindow(root)
+    app = MenuGeneral(root)
     root.mainloop()
